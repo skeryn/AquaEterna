42a5f696f46500d1ef755ac9d27512cee6b21b57
Skeryn <3925875+skeryn@users.noreply.github.com>
2022-04-02 23:38:57 +0200

    AquaEventDb update from AquaBot :heart: Version: 2022-04-02 23:38

---
+++ eventDb.lua
@@ -0,0 +1,15 @@
+AquaEventDb = {
+  {
+    ["name"] = "Sepulcre Suite",
+    ["guild"] = "Aqua eterna",
+    ["region"] = "eu",
+    ["locale"] = "en_US",
+    ["datetime"] = "2022-04-02 20:45",
+    ["signups"] = {
+      "Krullørg",
+      "Skeryn",
+      "Mzelledevill",
+      "Lollote"
+    }
+  }
+}
